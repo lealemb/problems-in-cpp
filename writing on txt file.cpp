05b547bb688ee7a16441808683ff4685ba218add
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-03-06 11:44:02 +0300

    Create writing on txt file.cpp

---
+++ writing on txt file.cpp
@@ -0,0 +1,15 @@
+//writing on a text file
+#include<iostream>
+using namespace std;
+int main()
+{
+ofstream myfile("example.txt")
+  if(myfile.is_open())
+  {
+    myfile << "This is a line.\n";
+    myfile << "This is another line.\n";
+    myfile.close();
+  }
+  else cout << "Unable to open file";
+  return 0;
+ }
