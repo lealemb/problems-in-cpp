1ed94a0e9cd0dc189bedbcf70477085ce35c9596
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-03-14 20:15:53 +0300

    Create sizeof data types.cpp

---
+++ sizeof data types.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"sizeof(char) is :"<<sizeof(char)<<endl; //1 bytes
+cout<<"The sizeof(short) is :"<<sizeof(short)<<endl; //2 bytes
+cout<<"The sizeof(int) is :"<<sizeof(int)<<endl; //4 bytes
+cout<<"The sizeof(long) is :"<<sizeof(long)<<endl; //8 bytes
+cout<<"The sizeof(long long) is :"<<sizeof(long long)<<endl; //8 bytes
+cout<<"The sizeof(float) is :"<<sizeof(float)<<endl; //4 bytes
+cout<<"The sizeof(double) is :"<<sizeof(double)<<endl; //8 bytes
+cout<<"The sizeof(long double) is :"<<sizeof(long double)<<endl; //16 bytes
+cout<<"The sizeof(bool) is :"<<sizeof(bool)<<endl; //1 bytes
+return 0;
+}
